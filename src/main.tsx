00530dac5c9533941887eaac014aac9f98e64c51
KSY <sye.g112@gmail.com>
2025-05-16 11:59:00 +0900

    추가: react-router로 기본 라우팅팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+const root = document.getElementById('root')
+
+if (!root) {
+  throw new Error('[오류] root element를 찾을 수 없습니다.')
+}
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
     <App />
-  </StrictMode>,
-)
+  </BrowserRouter>
+);
+
