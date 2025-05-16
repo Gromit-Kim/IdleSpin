d72b6fa5f57b6ea6a6df0f7d5da7704b7009dee4
KSY <sye.g112@gmail.com>
2025-05-16 13:27:19 +0900

    fix: header에 메인 페이지들이 가려지는 현상상

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import App from './App.tsx'
+import "./index.css"
 
 const root = document.getElementById('root')
 
