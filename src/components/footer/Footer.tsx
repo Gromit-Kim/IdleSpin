61bf4eca69b7e5abfeb3f50b0c623dbda2843abb
KSY <sye.g112@gmail.com>
2025-05-16 13:49:44 +0900

    추가: footer에 저작권 표시, 아이콘hover 기능 추가가

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,20 @@
+import "./footer.css";
+import linkedInIcon from "../../assets/icon-linkedIn.png";
+
 const Footer = () => {
     return (
-        <footer>
-            푸터
+        <footer className="footer">
+            <div className="footer-content">
+                <p className="copyright">
+                    &copy; 2025 Idle Spin. All right reserved.
+                </p>
+
+                <div className="social-buttons">
+                    <a href="#" target="_blank" rel="noopener noreferrer">
+                        <img src={linkedInIcon} alt="링크드인"/>
+                    </a>
+                </div>
+            </div>
         </footer>
     );
 }
