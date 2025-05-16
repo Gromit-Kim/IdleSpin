a1a35bbfe80c27dde2898beaa300ed582887b97d
KSY <sye.g112@gmail.com>
2025-05-16 11:58:01 +0900

    추가: home, routte, todo 페이지 추가가

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <div>
+            홈
+        </div>
+    );
+}
+
+export default Home;
+
---
+++ src/pages/Roulette.tsx
@@ -0,0 +1,9 @@
+const Roulette = () => {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Roulette;
